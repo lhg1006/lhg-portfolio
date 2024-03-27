be574314e10facc9cb6a8a4397ed5bde146c9526
이효규 <lhg961006@gmail.com>
2024-03-27 18:42:09 +0900

    작성폼 서버로 전송전까지. 커밋

--- app/write/page.tsx
+++ app/write/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useEffect, useRef, useState } from "react";
+import React, { useRef, useState } from "react";
 import Layout from "@/components/layout";
 import boardCss from "@/public/css/board.module.css";
 import { PrevImgList } from "@/types/photoType";
@@ -7,7 +7,18 @@ import { imageUpload } from "@/app/api/call/photo";
 import { toast } from "react-toastify";
 
 const Write = () => {
-    const MAX_FILE_COUNT = 5;
+    interface ImageData {
+        imagePath: string;
+    }
+
+    interface Content {
+        title: string | undefined;
+        images: ImageData[] | null;
+        texts: string[];
+    }
+
+    // const MAX_FILE_COUNT = 5;
+    const titleRef = useRef<HTMLInputElement | null>(null)
     const photoBaseUrl = process.env.REACT_APP_PHOTO_URL;
     const [contentList, setContentList] = useState<
         Array<{ image: PrevImgList | null; text: string }>
@@ -18,6 +29,15 @@ const Write = () => {
         if (e.target.files.length > 0) {
             onPhotoUpload(e.target.files[0], index);
         }
+    };
+
+    const handleChangeText = (e: React.ChangeEvent<HTMLTextAreaElement>, index: number) => {
+        const newText = e.target.value;
+        setContentList((prevList) => {
+            const updatedList = [...prevList];
+            updatedList[index].text = newText;
+            return updatedList;
+        });
     };
 
     // 이미지 업로드 및 에러 핸들러
@@ -53,6 +73,15 @@ const Write = () => {
             });
     };
 
+    // 이미지 삭제 핸들러
+    const handleDeleteImage = (index: number) => {
+        setContentList((prevList) => {
+            const updatedList = [...prevList];
+            updatedList[index].image = null; // 해당 인덱스의 이미지를 삭제
+            return updatedList;
+        });
+    };
+
     const addContent = () => {
         setContentList((prevList) => [
             ...prevList,
@@ -61,12 +90,28 @@ const Write = () => {
     };
 
     const addPost = () => {
+        // 이미지와 텍스트 값을 담을 배열 초기화
+        const images: ImageData[] = [];
+        const texts: string[] = [];
 
-    }
+        // contentList를 순회하며 이미지와 텍스트를 추출
+        contentList.forEach(content => {
+            if (content.image) {
+                images.push(content.image);
+            }
+            if (content.text) {
+                texts.push(content.text);
+            }
+        });
 
-    useEffect(() => {
-        console.log(contentList);
-    }, [contentList]);
+        const postData : Content = {
+            title: titleRef.current?.value,
+            images: images,
+            texts: texts,
+        };
+
+        console.log(postData)
+    };
 
     return (
         <>
@@ -74,12 +119,12 @@ const Write = () => {
                 <div className={boardCss.formContainer}>
                     <div className={boardCss.postForm}>
                         <h3 className={"mb-5 text-xl"}>작성</h3>
-                        <input type="text" placeholder="제목" />
+                        <input ref={titleRef} type="text" placeholder="제목" />
                         {contentList.map((content, index) => (
                             <div key={index} className="post">
                                 <div className={"mb-5"}>
                                     <label className={boardCss.fileInputButton}>
-                                        파일 선택
+                                        이미지 추가
                                         <input
                                             type="file"
                                             accept="image/*"
@@ -90,16 +135,10 @@ const Write = () => {
                                 <div className={boardCss.postContent}>
                                     {content.image && (
                                         <div className={boardCss.imageContainer}>
-                                            <div
-                                                className={boardCss.postPhotoBox}
-                                                style={{
-                                                    backgroundImage: `url(${
-                                                        process.env.NEXT_PUBLIC_PHOTO_URL +
-                                                        content.image.imagePath
-                                                    })`,
-                                                }}
-                                            />
-                                            <div className={boardCss.cancelButton}>X</div>
+                                            <div className={boardCss.prevPhotoBox}>
+                                                <img src={process.env.NEXT_PUBLIC_PHOTO_URL + content.image.imagePath} alt={'prevImg'}/>
+                                            </div>
+                                            <div className={boardCss.cancelButton} onClick={() => handleDeleteImage(index)}>X</div>
                                         </div>
                                     )}
                                     <textarea
@@ -109,6 +148,8 @@ const Write = () => {
                                             border: "1px solid #ccc",
                                         }}
                                         placeholder="내용을 입력하세요..."
+                                        value={content.text}
+                                        onChange={(e) => handleChangeText(e, index)}
                                     ></textarea>
                                 </div>
                             </div>
